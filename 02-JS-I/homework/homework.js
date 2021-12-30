1812b8a329292cf188f89e0920f885ea1ec080fc
valen benitez <benitezvalentin046@gmail.com>
2021-12-29 21:16:30 -0300

    three commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,40 +27,45 @@ const nuevoModulo = 21 % 5 === 1;
 
 function devolverString(str) {
   // "Return" la string provista: str
-  // Tu código:
-  
+  // Tu código: 
+  return(str)
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
+  // Tu código: 
+    return(x + y) 
   
 }
-
+  
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return(x -  y);
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return(x * y);
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
-  // Tu código:
-  
+  // Tu código: 
+  return(x / y);
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+   if (x == y){
+     return true;
+   } else{
+     return false;
+   }
 }
 
 function tienenMismaLongitud(str1, str2) {
@@ -74,20 +79,20 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90) {return true;} else{return false;}
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50) {return true;} else{return false;}
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  return(x % y);
 }
 
 function esPar(num) {
